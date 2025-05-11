a07f57f5111f65b65ade502467746ed2624aa07f
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-05-11 07:23:32 +0600

    admin py  🍅 ️

--- cart/admin.py
+++ cart/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Cart, CartItem
 # Register your models here.
-
+# Admin
 class CartAdmin(admin.ModelAdmin):
     list_display = ('cart_id', 'date_added')
 
